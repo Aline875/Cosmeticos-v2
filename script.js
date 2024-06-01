b1759840d3f6e13c689674c5309d337d8772fbcf
Aline <113183673+Aline875@users.noreply.github.com>
2024-06-01 15:26:16 -0300

    atualização
    Adição do botão de favoritar produto, cadastro de email para receber informações complementares, atualização da pagina de login e de cadastro.

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-function validarEmail(email)
-{
+function validarEmail(email) {
     const validacao = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return validacao.test(email)
+    return validacao.test(email);
 }
 
 function cadastrarEmail() {
     var email = document.getElementById("email").value;
 
-    if(validarEmail(email))
-        {
-            alert("Email cadastrado com sucesso!")
-        }
-        else
-        {
-            alert("Por favor, insira um email válido.")
-        }
+    if (validarEmail(email)) {
+        alert("Email cadastrado com sucesso!");
+    } else {
+        alert("Por favor, insira um email válido.");
+    }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Adiciona evento de clique para mostrar ou ocultar os cupons
+    document.querySelector('.topicos-link.cupons').addEventListener('click', function (event) {
+        event.preventDefault();
+        document.getElementById('cupons').classList.toggle('hidden');
+    });
+
+    // Adiciona evento de clique para mudar a imagem do botão de favoritar (like)
+    const botoesFavoritar = document.querySelectorAll('.btn-curtir');
+
+    botoesFavoritar.forEach(function (botao) {
+        botao.addEventListener('click', function () {
+            const imagemFavorito = botao.querySelector('img');
+            if (imagemFavorito.src.includes('/Public/icons8-coração-cheio-32.png')) {
+                imagemFavorito.src = '/Public/icons8-gostar-32.png';
+            } else {
+                imagemFavorito.src = '/Public/icons8-coração-cheio-32.png';
+            }
+        });
+    });
+});
